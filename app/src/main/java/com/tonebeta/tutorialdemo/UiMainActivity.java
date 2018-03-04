d534a69c4645d68f6c28130fd726dbe2eeb8e107
tonebeta <tonebeta@gmail.com>
2018-03-04 17:55:24 +0800

    add support library for TabLayout

--- app/src/main/java/com/tonebeta/tutorialdemo/UiMainActivity.java
+++ app/src/main/java/com/tonebeta/tutorialdemo/UiMainActivity.java
@@ -1,11 +1,23 @@
 package com.tonebeta.tutorialdemo;
 
-import android.support.v7.app.AppCompatActivity;
-
 /**
  * Created by 109890 on 2018/3/4.
  */
 
-public class UiMainActivity extends AppCompatActivity {
+public class UiMainActivity extends BaseActivity {
 
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
 }
